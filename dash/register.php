2e4ec26f28a185dfe07416e8a5ae7d3edf7d1788
Tosiek0 <nieantek111@icloud.com>
2025-11-12 08:32:19 +0100

    bootstrap update

--- dash/register.php
+++ dash/register.php
@@ -44,11 +44,11 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <title>Rejestracja</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/login_style.css">
+    <link href="css/boot.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<div class="login-card">
+<div class="register-card">
     <h2 class="text-center mb-4">Rejestracja</h2>
     <form method="POST" action="register.php">
         <div class="mb-3">
@@ -73,6 +73,5 @@ $conn->close();
         <small>Masz już konto? <a href="login.php">Zaloguj się</a></small>
     </div>
 </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
